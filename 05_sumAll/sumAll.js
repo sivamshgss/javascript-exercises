89955f910e616d0320cffde1d5e9f3b1b5e7d3aa
sivamshgss <101281718+sivamshgss@users.noreply.github.com>
2025-02-25 00:09:49 +0530

    Complete Exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
-
+const sumAll = function(number1, number2) {
+    if (typeof number1 !== "number" || typeof number2 !== "number" || number1 < 0 || number2 < 0 || number1 !== parseInt(number1) || number2 !== parseInt(number2)) {
+        return "ERROR";
+    }
+    let sum = 0;
+    for (let i = Math.min(number1, number2); i <= Math.max(number1, number2); ++i) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
