87c15954fd6515fbf0991df84933e39a4e34d4ae
sivamshgss <101281718+sivamshgss@users.noreply.github.com>
2025-02-24 23:59:38 +0530

    Complete Exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...del) {
+    let i = 0;
+    while (i < arr.length) {
+        if (del.includes(arr[i])) {
+            arr.splice(i, 1);
+            continue;
+        } else {
+            ++i;
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
